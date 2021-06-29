2961e9d8d27b42633ac2cc8217d501d6acf041a1
Alexandr <alexandr.mirz12@gmail.com>
2021-06-29 10:45:15 +0400

    google copyright comments removed from files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,9 +41,3 @@ import { CounterComponent } from './counter/counter.component';
   providers: [CartService]
 })
 export class AppModule {}
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
